cb424cc67f425237738f5f581717012c1826a1e2
Vladimir <treshskate@mail.ru>
2022-10-05 12:29:18 +0300

    007

--- 007/Program.cs
+++ 007/Program.cs
@@ -9,5 +9,4 @@ Console.Clear();
 int num = new Random().Next(100, 1000);
 int a1 = num / 100;
 int a2 = num % 10;
-
-Console.WriteLine($"Первая и последние цифры числа {num} ->{a1}{a2}");
+Console.WriteLine($"Первая и последние цифры числа {num} ->{a1 * 10 + a2}");
