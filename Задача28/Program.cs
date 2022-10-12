abc876d43a9626104359f508d562e0c689e72070
Vladimir <treshskate@mail.ru>
2022-10-12 13:15:28 +0300

    Задача28

---
+++ Задача28/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+
+int GetMulti(int n)
+{
+    int multi = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        multi = multi * i;
+    }
+    return multi;
+}
+Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int result = GetMulti(N);
+Console.WriteLine(result);
+
