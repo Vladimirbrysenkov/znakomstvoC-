6bd3bce8b060285e0e08aca8aec345f9a4d17285
Vladimir <treshskate@mail.ru>
2022-10-12 15:52:19 +0300

    Задача29

--- Задача29/Program.cs
+++ Задача29/Program.cs
@@ -2,19 +2,29 @@
 
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
-
-int[] numbers = new int[8];
+Console.Write("Введите количество элементов массива: ");
+int elementCount = int.Parse(Console.ReadLine()!);
+int[] numbers = new int[elementCount];
 Console.Write("[");
-for (int i = 0; i < numbers.Length; i++)
+if (elementCount <= 8)
 {
-    numbers[i] = new Random().Next(0, 99);
-    Console.Write(" " + Method(i) + " ");
-    if (i != numbers.Length - 1)
+    for (int i = 0; i < numbers.Length; i++)
     {
-        Console.Write(", ");
+        numbers[i] = new Random().Next(0, 99);
+        Console.Write(" " + Method(i) + " ");
+        if (i != numbers.Length - 1)
+        {
+            Console.Write(", ");
+        }
     }
 }
 Console.Write("]");
+if (elementCount == 0)
+{
+    Console.Write("Ноль элементов массива!");
+}
+if (elementCount > 8)
+Console.Write("Ошибка! Введите правильное количество элементов массива!");
 int Method(int a)
 {
     return numbers[a];
