60fbf2659c97bdee2a83a8ccc07f03c18a8aedc7
Vladimir <treshskate@mail.ru>
2022-10-21 19:54:42 +0300

    Задача45

---
+++ Задача45/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая будет создавать
+//  копию заданного массива с помощью поэлементного копирования. 
+
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+int[] NewArray2(int[] inArray)
+{
+    int[] result = new int[inArray.Length];
+
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        result[i] = inArray[i];
+    }
+    return result;
+}
+Console.Clear();
+Console.WriteLine("Введите массив через пробел");
+string input = Console.ReadLine();
+int[] numArray = GetArrayFromString(input);
+Console.WriteLine(String.Join(", ", numArray));
