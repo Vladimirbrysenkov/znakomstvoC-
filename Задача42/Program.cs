16aafe7ca84b9fecf646a47036d777097e69cc98
Vladimir <treshskate@mail.ru>
2022-10-21 11:00:49 +0300

    Задача42

---
+++ Задача42/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая будет преобразовыватьидесятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.Clear();
+
+string NewMass(int a)
+{
+    string arr = "";
+    while (a > 0)
+    {
+        arr += (a % 2).ToString();
+        a /= 2;
+    }
+    return arr;
+}
+
+int MassRev(string arr)
+{
+    string rezult = "";
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        rezult += arr[arr.Length - 1 - i];
+    }
+    return int.Parse(rezult);
+}
+
+Console.WriteLine("Введите десятичное число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+string array = NewMass(num);
+int num1 = MassRev(array);
+Console.Write($"В двоичном виде: {num1} ");
