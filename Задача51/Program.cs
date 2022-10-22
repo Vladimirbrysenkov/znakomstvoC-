637c379f82b7261b6a41720680028cfd7f080269
Vladimir <treshskate@mail.ru>
2022-10-22 12:11:54 +0300

    Задача51

---
+++ Задача51/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задайте двумерный массив. Найдите сумму элементов, 
+// находящихся на главной диагонали (синдексами (0,0); (1;1) и т.д.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+int[,] GetArray(int m, int n)
+{
+    int[,] inArray = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+        {
+            inArray[i, j] = new Random().Next(0, 21);
+        }
+    return inArray;
+}
+void PrintArray(int[,] inArray)
+{
+
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+int SumOfMainDiag(int[,] inArray)
+{
+    int sum = 0;
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (i == j) sum = sum + inArray[i, j];
+        }
+    }
+     return sum;
+}
+Console.Clear();
+Console.Write("Введите количество строк в массиве: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(row, columns);
+PrintArray(array);
+Console.WriteLine();
+SumOfMainDiag(array);
+Console.Write($"сумма элементов, находящихся на главной диагонали = {SumOfMainDiag(array)} ");
