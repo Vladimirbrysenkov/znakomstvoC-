36d1acd3040fda4d492ca58c502b0fd7f074bf22
Vladimir <treshskate@mail.ru>
2022-11-14 14:12:57 +0300

    Задача65

---
+++ Задача65/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+
+string GetPrint(int start, int end)
+{
+    if (start == end)
+    {
+        return start.ToString();
+    }
+    Console.WriteLine($"{start}");
+    return start + " " + GetPrint(start + 1, end);
+}
+Console.Clear();
+Console.Write("Введите чило M ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите чило N ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(GetPrint(m, n));
