d000d4bfaccb76fc2963fe8e9ec7fcc4a3b65854
Vladimir <treshskate@mail.ru>
2022-11-14 14:07:26 +0300

    Задача63

---
+++ Задача63/Program.cs
@@ -0,0 +1,16 @@
+﻿// 
+
+string GetPrint(int start, int end)
+{
+    if (start == end)
+    {
+        return start.ToString();
+    }
+    Console.WriteLine($"f({start}, {end}) -> {start + " " + GetPrint(start + 1, end)}");
+    return start + " " + GetPrint(start + 1, end);
+}
+
+Console.Write("Введите чило N ");
+int n = int.Parse(Console.ReadLine()!);
+int m = 1;
+Console.WriteLine(GetPrint(m, n));
